73e9c157a9d08b7c19b5110593e277af9f7585ce
HuzaifaKhalid2 <huzaifa.khalid@ocsbstudent.ca>
2021-01-01 00:50:07 +0000

    fixed javascript format and added css code

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 let l = 12
-l = 'length of square = 12cm';
-
-alert(l); // shows the variable content
-
- var area = l^l;
- area = 'area (or a) of square = l^l';
- 
- alert(area); // shows the variable content
-
-let a = 12^12;
+l = 'length of square = 12cm'
+alert(l)
+let area = l^l;
+area = 'area (or a) of square = l^l';
+alert(area)
+let a = 12^12
 a = 'a = 12^12'
-
-alert(a); // shows the variable content
-
-var answer = 'a = 144cm2';
-
-alert(answer); // shows the variable content
-
+alert(a)
+let answer = '= 144cm2'
+alert(answer)
 let statment = 'Therefore the area of this square is 144cm'
 statment = 'Therefore the area of this square is 144cm squared'
