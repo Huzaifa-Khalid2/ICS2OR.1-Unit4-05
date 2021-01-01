42fd424be54476169b447732c9a61af591070210
HuzaifaKhalid2 <huzaifa.khalid@ocsbstudent.ca>
2021-01-01 00:56:30 +0000

    fixed javascript format and readme format

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ICS2OR.1-Unit4-05
-[![linter](https://github.com/Huzaifa-Khalid2/ICS2OR.1-Unit4-05/workflows/linter/badge.svg)](https://github.com/marketplace/actions/super-linter)  
+[![linter](https://github.com/Huzaifa-Khalid2/ICS2OR.1-Unit4-05/workflows/linter/badge.svg)](https://github.com/marketplace/actions/super-linter)
--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let l = 12
 l = 'length of square = 12cm'
 alert(l)
-let area = l^l;
-area = 'area (or a) of square = l^l';
+let area = l ^ l
+area = 'area (or a) of square = l^l'
 alert(area)
-let a = 12^12
-a = 'a = 12^12'
+let a = '12 ^ 12'
+a = 'a = 12 ^ 12'
 alert(a)
 let answer = '= 144cm2'
+answer = 'a = 144cm2'
 alert(answer)
 let statment = 'Therefore the area of this square is 144cm'
 statment = 'Therefore the area of this square is 144cm squared'
+alert(statment)
